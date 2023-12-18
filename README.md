2100c80ef469115b7be24405f026a7287e1dd4b1
ulfschneider <mail@ulfschneider.io>
2023-12-18 10:26:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Eleventy plugin to easily embed YouTube videos with a minimal footprint
+
+The plugin leverages [Paul Irish´s lite-youtube web component](https://github.com/paulirish/lite-youtube-embed).
+
+The typical YouTube embed has a weight above 1 MB, while Paul Irish´s `<lite-youtube>` comes with a total weight of 32 KB (which is about 2-3 % of the original size). This way pages load _much faster_ and consume _much less_ bandwidth. And, as a bonus, it´s easier to embed a YouTube video – dropping the URL in a dedicated textline is enough.
+
+Please also refer to [ONE YOUTUBE EMBED WEIGHS ALMOST 1.2 MB](https://www.zachleat.com/web/youtube-embeds/)
+
+## Install
+
+```sh
+npm install --save-dev eleventy-plugin-lite-youtube
+```
+
+In your `eleventy.js` file:
+
+```js
+const liteYoutube = require("eleventy-plubin-lite-youtube");
+//...
+eleventyConfig.addPlugin(liteYoutube);
+```
+
+## Usage
+
+The plugin will replace the following occurrences in your content with the `<lite-youtube>` web component:
+
+- A YouTube url within a single line of your markdown
+- A YouTube url enclosed by `<p></p>`
+- A YouTube url enclosed by `<figure></figure>` (`<figcaption>` is fine)
+- A YouTube embedding `<iframe>`
---
+++ eleventy.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const { minify } = require("terser");
+const CleanCSS = require("clean-css");
+
+let minifiedYouTubeJavaScript;
+let minifiedYouTubeCSS;
+
+async function getYouTubeJavaScript() {
+  if (!minifiedYouTubeJavaScript) {
+    const liteYouTubeJavaScript = fs
+      .readFileSync("./node_modules/lite-youtube-embed/src/lite-yt-embed.js")
+      .toString();
+    minifiedYouTubeJavaScript = (
+      await minify(liteYouTubeJavaScript, { keep_classnames: true })
+    ).code;
+  }
+  return minifiedYouTubeJavaScript;
+}
+
+function getYouTubeCSS() {
+  if (!minifiedYouTubeCSS) {
+    const liteYouTubeCSS = fs
+      .readFileSync("./node_modules/lite-youtube-embed/src/lite-yt-embed.css")
+      .toString();
+    minifiedYouTubeCSS = new CleanCSS({}).minify(liteYouTubeCSS).styles;
+  }
+  return minifiedYouTubeCSS;
+}
+
+function replaceHTMLWithLiteYoutube(content, index, length, videoId) {
+  return (
+    content.substring(0, index) +
+    `<lite-youtube videoid="${videoId}"></lite-youtube>` +
+    content.substring(index + length)
+  );
+}
+
+async function hydrateLiteYoutube(content) {
+  content += `\n<style>${getYouTubeCSS()}</style>`;
+  content += `\n<script>${await getYouTubeJavaScript()}</script>`;
+  return content;
+}
+
+async function transformLiteYoutube(content) {
+  const IFRAME =
+    /<iframe\s+.*?src="\S*?(youtube\.com|youtu\.be)\/(embed\/|watch\?v=)?(?<videoId>[^?<\s]+).*?\s*?<\/iframe>/i;
+  const SINGLE_LINE =
+    /<p(\s+.*?)?>\s*(http(s)?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(embed\/|watch\?v=)?(?<videoId>[^?<\s]+).*?\s*<\/p>/i;
+  const FIGURE =
+    /<figure(\s+.*?)?>\s*(http(s)?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(embed\/|watch\?v=)?(?<videoId>[^?<\s]+).*?\s*?(<\/figure>|<figcaption>)/i;
+
+  const PATTERN = [IFRAME, SINGLE_LINE, FIGURE];
+
+  let found = false;
+  for (const pattern of PATTERN) {
+    while ((match = content.match(pattern))) {
+      found = true;
+      content = replaceHTMLWithLiteYoutube(
+        content,
+        match.index,
+        match[0].length,
+        match.groups.videoId
+      );
+    }
+  }
+
+  if (found) {
+    content = await hydrateLiteYoutube(content);
+  }
+
+  return content;
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addTransform("lite-video", async function (content) {
+    return await transformLiteYoutube(content);
+  });
+};
